18728b9c84a9dec8c612569c04e04d9ad14eb101
tiborf <filetibor2002@gmail.com>
2023-06-21 20:20:01 +0200

    Protohackers Problem 0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = tokio::net::TcpListener::bind("192.168.1.200:12730").await?;
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = stream.read(&mut buf).await.expect("Couldn't read");
+                if n == 0 { break; }
+                stream.write_all(&buf[0..n]).await.expect("Couldn't write");
+            }
+        });
+    }
 }
